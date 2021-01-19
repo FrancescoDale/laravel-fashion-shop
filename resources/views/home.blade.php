11fc1e411745f6a552092ca12ce54644f9bc149a
FrancescoDale <fd060862@gmail.com>
2021-01-19 16:00:59 +0100

    test funzionamento controller

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,15 +1,5 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Home</title>
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+@extends('layouts.app')
 
-    </head>
-    <body>
+@section('content')
 
-
-    </body>
-</html>
+@endsection
