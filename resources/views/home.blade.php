0521840938e65df0a36ebf3a2b1ac899c2275aa2
FrancescoDale <fd060862@gmail.com>
2021-01-19 17:36:57 +0100

    stampa degli oggetti Dress

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,14 @@
         <h1 class="text-capitalize text-center mt-4">prodotti in magazzino</h1>
         <div class="text-capitalize mt-3">
             @foreach ($dresses as $dress)
-                <p class="mt-4">{{ $dress->fabric }}</p>
+                <div class="mt-4">
+                    <h4 class="text-uppercase">prodotto {{ $dress->id}}</h4>
+                    <p>{{ $dress->fabric }}</p>
+                    <p>{{ $dress->size }}</p>
+                    <p>{{ $dress->color }}</p>
+                    <p>{{ $dress->price }} €</p>
+
+                </div>
             @endforeach
         </div>
 
